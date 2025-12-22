03658658d7bc79b5cf12d2fd47970ad2f46550ef
Santi <santi@Mac-mini-de-Santi.local>
2025-12-22 14:50:21 -0500

    Fix widget duplicate default export

--- app/widget/page.tsx
+++ app/widget/page.tsx
@@ -135,12 +135,5 @@ export default function Widget() {
     </div>
   )
 }
-export default function Widget() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Widget OK</h1>
-      <p>Si ves esto, /widget ya existe.</p>
-    </div>
-  )
-}
+
 
